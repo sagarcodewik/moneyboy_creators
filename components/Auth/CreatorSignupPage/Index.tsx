be0d1f978a82b832c86f6a0733b4b0bfb15d9f58
sagarcodewik <sagar.codewik@gmail.com>
2025-12-09 15:46:33 +0530

    Dropdown

--- components/Auth/CreatorSignupPage/Index.tsx
+++ components/Auth/CreatorSignupPage/Index.tsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { TbCamera } from "react-icons/tb";
+import CustomSelect from "@/components/CustomSelect";
 
 const CreatorSignupPage = () => {
   const [activeTab, setActiveTab] = useState("fan");
   const [showPass, setShowPass] = useState(false);
   const [showConfirmPass, setShowConfirmPass] = useState(false);
+
   return (
     <div className="bg-off-white">
      <div className="container login_wrap creator_wrap">
@@ -118,94 +120,25 @@ const CreatorSignupPage = () => {
                  <div className="input-placeholder-icon"><svg className="icons messageUser svg-icon"></svg></div>
                   <textarea rows="4"  placeholder="Bio"></textarea>
                 </div>
-                <div className="custom-select-element" data-custom-select-element="" data-custom-select-value="">
-                  <div className="custom-select-label-wrapper" data-custom-select-triger="">
-                    <div className="custom-select-icon-txt">                   
-                      <div className="input-placeholder-icon"><svg className="icons handbody svg-icon"></svg></div>
-                      <span className="custom-select-label-txt">All Body Types</span>
-                    </div>
-                    <div className="custom-select-chevron"><svg className="icons chevronDown svg-icon"></svg></div>
-                  </div>
-                  <div className="custom-select-options-dropdown-wrapper" data-custom-select-dropdown="">
-                    <div className="custom-select-options-dropdown-container">
-                      <div className="custom-select-options-search" data-custom-select-options-search="">
-                        <div className="label-input">
-                          <div className="input-placeholder-icon">
-                            <svg className="svg-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                              <path d="M20 11C20 15.97 15.97 20 11 20C6.03 20 2 15.97 2 11C2 6.03 6.03 2 11 2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                              </path>
-                              <path d="M18.9299 20.6898C19.4599 22.2898 20.6699 22.4498 21.5999 21.0498C22.4499 19.7698 21.8899 18.7198 20.3499 18.7198C19.2099 18.7098 18.5699 19.5998 18.9299 20.6898Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                              </path>
-                              <path d="M14 5H20" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                              <path d="M14 8H17" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                            </svg>
-                          </div>
-                          <input type="text" placeholder="Search here"/>
-                        </div>
-                      </div>
-                      <div className="custom-select-options-lists-container">
-                        <ul className="custom-select-options-list" data-custom-select-options-list="">
-                          <li className="custom-select-option"><span>Option 1</span></li>
-                          <li className="custom-select-option"><span>Option 2</span></li>
-                          <li className="custom-select-option"><span>Option 3</span></li>
-                          <li className="custom-select-option"><span>Option 4</span></li>
-                          <li className="custom-select-option"><span>Option 5</span></li>
-                          <li className="custom-select-option"><span>Option 6</span></li>
-                          <li className="custom-select-option"><span>Option 7</span></li>
-                          <li className="custom-select-option"><span>Option 8</span></li>
-                          <li className="custom-select-option"><span>Option 9</span></li>
-                          <li className="custom-select-option"><span>Option 10</span></li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+
+                <CustomSelect label="All Body Types" icon={<svg className="icons handbody svg-icon" />}
+                 options={[
+                  { label: "All Body Types", value: "all" },
+                  { label: "18-24", value: "18-24" },
+                  { label: "25-34", value: "25-34" },
+                  { label: "35-44", value: "35-44" },
+                  { label: "45+", value: "45plus" },
+                ]}/>
                 <div className="label-input">
                   <div className="input-placeholder-icon"><svg className="icons timeIcon svg-icon"></svg></div>
                   <input type="text" placeholder="All Sexual Orientation *"/>
                 </div>
-                <div className="custom-select-element" data-custom-select-element="" data-custom-select-value="">
-                  <div className="custom-select-label-wrapper" data-custom-select-triger="">
-                    <div className="custom-select-icon-txt"> 
-                      <svg className="icons calendarClock svg-icon"></svg>
-                      <span className="custom-select-label-txt">All Ages</span>
-                    </div>
-                    <div className="custom-select-chevron"><svg className="icons chevronDown svg-icon"></svg></div>
-                  </div>
-                  <div className="custom-select-options-dropdown-wrapper" data-custom-select-dropdown="">
-                    <div className="custom-select-options-dropdown-container">
-                      <div className="custom-select-options-search" data-custom-select-options-search="">
-                        <div className="label-input">
-                          <div className="input-placeholder-icon">
-                            <svg className="svg-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                              <path d="M20 11C20 15.97 15.97 20 11 20C6.03 20 2 15.97 2 11C2 6.03 6.03 2 11 2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                              </path>
-                              <path d="M18.9299 20.6898C19.4599 22.2898 20.6699 22.4498 21.5999 21.0498C22.4499 19.7698 21.8899 18.7198 20.3499 18.7198C19.2099 18.7098 18.5699 19.5998 18.9299 20.6898Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                              </path>
-                              <path d="M14 5H20" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                              <path d="M14 8H17" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                            </svg>
-                          </div>
-                          <input type="text" placeholder="Search here"/>
-                        </div>
-                      </div>
-                      <div className="custom-select-options-lists-container">
-                        <ul className="custom-select-options-list" data-custom-select-options-list="">
-                          <li className="custom-select-option"><span>Option 1</span></li>
-                          <li className="custom-select-option"><span>Option 2</span></li>
-                          <li className="custom-select-option"><span>Option 3</span></li>
-                          <li className="custom-select-option"><span>Option 4</span></li>
-                          <li className="custom-select-option"><span>Option 5</span></li>
-                          <li className="custom-select-option"><span>Option 6</span></li>
-                          <li className="custom-select-option"><span>Option 7</span></li>
-                          <li className="custom-select-option"><span>Option 8</span></li>
-                          <li className="custom-select-option"><span>Option 9</span></li>
-                          <li className="custom-select-option"><span>Option 10</span></li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <CustomSelect label="All Ages" icon={<svg className="icons calendarClock svg-icon"></svg>}
+                  options={[
+                    { label: "18", value: "18" },
+                    { label: "25", value: "25" },
+                ]}/>
+
                 <div className="custom-select-element" data-custom-select-element="" data-custom-select-value="">
                   <div className="custom-select-label-wrapper" data-custom-select-triger="">
                     <div className="custom-select-icon-txt">
